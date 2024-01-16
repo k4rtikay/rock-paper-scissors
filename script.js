93f1cf44a26f32d85997bccc8fc6c018834fac40
Kartikeya Mishra <katt1bh4@gmail.com>
2024-01-16 08:02:12 +0530

    Start to add ui to the rock paper scissors game add buttons for player choice and add a scoreboard and message div all from the script

--- script.js
+++ script.js
@@ -10,63 +10,106 @@ function getComputerChoice(){
     return 'scissors';
 }
 
-let cntp =0;
-let cntc =0;
-
 function game(){
-    cntp=0;
-    cntc=0;
-    for(i=1;i<=5;i++){
-    let playerChoice = prompt("enter your choice","rock");
-    let computerChoice= getComputerChoice();
-    console.log(playRound(computerChoice,playerChoice));
-    }
-    if(cntc>cntp){
-        console.log(`you lose!! SCORE: ${cntp}-${cntc}` );
-    }else if(cntc==cntp){
-        console.log(`its a draw!! SCORE: ${cntp}-${cntc}`);
+    let cntp=0;
+    let cntc=0;
+    if(cntp<5&&cntc<5){
+        getPlayerChoice();
+        getScore();
     }else{
-        console.log(`you win!! SCORE: ${cntp}-${cntc}`)
+      if(cntc>cntp){
+          console.log(`you lose!! SCORE: ${cntp}-${cntc}` );
+      }else if(cntc==cntp){
+        console.log(`its a draw!! SCORE: ${cntp}-${cntc}`);
+      }else{
+          console.log(`you win!! SCORE: ${cntp}-${cntc}`);
+      }
     }
+
+
+    function getPlayerChoice(){
+    btn1.addEventListener('click',()=>{
+        disp.textContent=playRound(getComputerChoice(),'rock');
+    })
+    btn2.addEventListener('click',()=>{
+        disp.textContent=playRound(getComputerChoice(),'paper');
+    })
+    btn3.addEventListener('click',()=>{
+        disp.textContent=playRound(getComputerChoice(),'scissors');
+    })
+    }
+
     function playRound(computerChoice, playerChoice){
-        const p = playerChoice.toLowerCase();
-        const w = `Congratulations you win!!! ${playerChoice} beats ${computerChoice}`;
-        const l = `you lost!!! ${computerChoice} beats ${playerChoice}`;
-        if(p===computerChoice){
-            return `its a tie!!! ${playerChoice} draws with ${computerChoice}`
-        }
-        if(p==='rock'){
-            if(computerChoice=='paper'){
-                cntc++;
-                return l;
-            }
-            else if(computerChoice=='scissors'){
-                cntp++;
-                return w;
-            }
-        }
-        if(p==='paper'){
-            if(computerChoice=='rock'){
-                cntp++;
-                return w;
-            }
-            else if(computerChoice==='scissors'){
-                cntc++;
-                return l;
-            }
-        }
-        if(p==='scissors'){
-            if(computerChoice=='paper'){
-                cntp++;
-                return w;
-            }
-            else if(computerChoice==='rock'){
-                cntc++;
-                return l;
-            }
-        }
-        return 'enter a valid choice!';
+    const p = playerChoice.toLowerCase();
+    const w = `Congratulations you win!!! ${playerChoice} beats ${computerChoice}`;
+    const l = `you lost!!! ${computerChoice} beats ${playerChoice}`;
+    
+    if(p===computerChoice){
+        score.textContent=`${cntp}-${cntc}`;
+        return `its a tie!!! ${playerChoice} draws with ${computerChoice}`
     }
+    if(p==='rock'){
+        if(computerChoice=='paper'){
+            cntc++;
+            score.textContent=`${cntp}-${cntc}`;
+            return l;
+        }
+        else if(computerChoice=='scissors'){
+            cntp++;
+            score.textContent=`${cntp}-${cntc}`;
+            return w;
+        }
+    }
+    if(p==='paper'){
+        if(computerChoice=='rock'){
+            cntp++;
+            score.textContent=`${cntp}-${cntc}`;
+            return w;
+        }
+        else if(computerChoice==='scissors'){
+            cntc++;
+            score.textContent=`${cntp}-${cntc}`;
+            return l;
+        }
+    }
+    if(p==='scissors'){
+        if(computerChoice=='paper'){
+            cntp++;
+            score.textContent=`${cntp}-${cntc}`;
+            return w;
+        }
+        else if(computerChoice==='rock'){
+            cntc++;
+            score.textContent=`${cntp}-${cntc}`;
+            return l;
+        }
+    }
+    return 'enter a valid choice!';
 }
+
+
+}
+
+
+
+const container = document.querySelector('.container');
+
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const btn3 = document.createElement('button');
+const disp = document.createElement('div');
+const score = document.createElement('div');
+
+btn1.textContent='ROCK';
+btn2.textContent='PAPER';
+btn3.textContent='SCISSORS';
+
+
+
+container.appendChild(btn1);
+container.appendChild(btn2);
+container.appendChild(btn3);
+container.appendChild(disp);
+container.appendChild(score);
 
 game();
